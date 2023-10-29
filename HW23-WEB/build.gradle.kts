ba84bd90116321c6cd1b0c47654ada4b205c5ac3
Андрей Овчаренко <a@krot.name>
2023-10-29 10:37:16 +0300

    HW23 jetty work

--- HW23-WEB/build.gradle.kts
+++ HW23-WEB/build.gradle.kts
@@ -13,9 +13,19 @@ dependencies {
     annotationProcessor ("org.projectlombok:lombok")
 
     implementation("ch.qos.logback:logback-classic")
+    implementation("com.google.code.gson:gson")
     implementation("org.hibernate.orm:hibernate-core")
     implementation("org.flywaydb:flyway-core")
     implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.17")
+    implementation("org.eclipse.jetty:jetty-server:11.0.17")
+    implementation("org.eclipse.jetty:jetty-webapp:11.0.17")
+    implementation("org.eclipse.jetty:jetty-security:11.0.17")
+    implementation("org.eclipse.jetty:jetty-http:11.0.17")
+    implementation("org.eclipse.jetty:jetty-io:11.0.17")
+    implementation("org.eclipse.jetty:jetty-util:11.0.17")
+    implementation("org.freemarker:freemarker")
 
 
     implementation("org.postgresql:postgresql")
--- HW23-WEB/src/main/java/ru/otus/DbServiceDemo.java
+++ HW23-WEB/src/main/java/ru/otus/DbServiceDemo.java
@@ -1,8 +1,9 @@
-package ru.otus.demo;
+package ru.otus;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.cfg.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.otus.core.repository.DataTemplateHibernate;
 import ru.otus.core.repository.HibernateUtils;
 import ru.otus.core.sessionmanager.TransactionManagerHibernate;
@@ -11,17 +12,26 @@ import ru.otus.crm.model.Address;
 import ru.otus.crm.model.Client;
 import ru.otus.crm.model.Phone;
 import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.servlet.TemplateProcessor;
+import ru.otus.servlet.TemplateProcessorImpl;
+import ru.otus.servlet.UserAuthService;
+import ru.otus.servlet.UserAuthServiceImpl;
+import ru.otus.web.UsersWebServer;
+import ru.otus.web.UsersWebServerWithFilterBasedSecurity;
 
 import java.util.List;
 
+import static ru.otus.web.UsersWebServerWithFilterBasedSecurity.TEMPLATES_DIR;
+
+@Slf4j
 public class DbServiceDemo {
 
-    private static final Logger log = LoggerFactory.getLogger(DbServiceDemo.class);
+    private static final int WEB_SERVER_PORT = 5555;
 
 
     public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
 
         var dbUrl = configuration.getProperty("hibernate.connection.url");
@@ -33,9 +43,9 @@ public class DbServiceDemo {
         var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, Phone.class, Address.class);
 
         var transactionManager = new TransactionManagerHibernate(sessionFactory);
-///
+
         var clientTemplate = new DataTemplateHibernate<>(Client.class);
-///
+
         var dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
         dbServiceClient.saveClient(new Client("dbServiceFirst"));
 
@@ -43,7 +53,7 @@ public class DbServiceDemo {
         var clientSecondSelected = dbServiceClient.getClient(clientSecond.getId())
                 .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
         log.info("clientSecondSelected:{}", clientSecondSelected);
-///
+
         dbServiceClient.saveClient(new Client(clientSecondSelected.getId(), "dbServiceSecondUpdated"));
         var clientUpdated = dbServiceClient.getClient(clientSecondSelected.getId())
                 .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecondSelected.getId()));
@@ -56,16 +66,11 @@ public class DbServiceDemo {
         var client = new Client(null, "Vasya", new Address(null, "AnyStreet"), List.of(new Phone(null, "13-555-22"),
                 new Phone(null, "14-666-333")));
 
-        //when
         var savedClient = transactionManager.doInTransaction(session -> {
             clientTemplate.insert(session, client);
             return client;
         });
 
-        //then
-
-
-        //when
         var loadedSavedClient = transactionManager.doInReadOnlyTransaction(
                 session -> clientTemplate.findById(session, savedClient.getId())
                         .map(Client::clone)
@@ -73,5 +78,13 @@ public class DbServiceDemo {
 
         System.out.println(loadedSavedClient);
 
+
+        UserAuthService authService = new UserAuthServiceImpl();
+
+        UsersWebServer usersWebServer = new UsersWebServerWithFilterBasedSecurity(WEB_SERVER_PORT,
+                authService);
+
+        usersWebServer.start();
+        usersWebServer.join();
     }
 }
