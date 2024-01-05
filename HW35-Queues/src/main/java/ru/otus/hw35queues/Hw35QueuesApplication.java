be4388f851a8f97f1071956f51c2d46115c2da33
Андрей Овчаренко <a@krot.name>
2024-01-05 15:37:37 +0300

    HW35-Queues init

---
+++ HW35-Queues/src/main/java/ru/otus/hw35queues/Hw35QueuesApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw35queues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw35QueuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw35QueuesApplication.class, args);
+    }
+
+}
